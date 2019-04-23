0fdf33db7f9e0f0e84efd91a6d2fd5da836a3597
root <root@ct85.localdomain>
2019-04-23 16:35:26 +0900

    (1) DataGridView 성능(FAKE), (2) 소요시간 등

---
+++ XLog/PUBLIC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLog
+{
+    public class PUBLIC
+    {
+        public static double TIME_DELTA { get; set; }
+        private static long mTickPrev { get; set; } = -1;
+        private static long mTickStart { get; set; } = -1;
+
+        public static double TIME_CHECK(long aTickStart = -1)
+        {
+            long sTickNow = System.DateTime.Now.Ticks;
+
+            if (aTickStart != -1)
+            {
+                mTickStart = aTickStart;
+                return 0;
+            }
+
+            if (mTickPrev == -1)
+            {
+                TIME_DELTA = 0;
+            }
+            else
+            {
+                TIME_DELTA = Math.Truncate((double)(sTickNow - mTickPrev) / 10000.0F) / 1000;
+            }
+            mTickPrev = sTickNow;
+
+            return Math.Truncate((double)(sTickNow - mTickStart) / 10000.0F) / 1000;
+        }
+
+    }
+
+}
